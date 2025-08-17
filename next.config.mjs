2f5d7dea2d3558026a34bf9baaa7b95f41d621e4
GotYourWallet <philipp.schilcher@hotmail.com>
2025-08-17 17:56:43 +0200

    testing github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-// next.config.mjs
+// next.config.mjs testing
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone", //enables smaller docker images, however it requires additional configuration in package.json
